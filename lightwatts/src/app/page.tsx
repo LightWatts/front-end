2c66db15e74e017a4e674a507aeba592cc6dc869
jvrochacandido@gmail.com <joaovitorbrogginelopes@gmail.com>
2024-11-12 22:01:58 -0300

    Feita a homepage, ainda não responsivisada.

--- lightwatts/src/app/page.tsx
+++ lightwatts/src/app/page.tsx
@@ -1,3 +1,5 @@
+import CardSlider from "@/components/card";
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
 import Link from "next/link";
@@ -21,12 +23,12 @@ export default function Home() {
         </div>
       </section>
       <article className="text-black mt-16 ml-16 mb-16">
-        <h1 className="text-4xl font-lulo">POR QUE EXISTIMOS?</h1>
+        <h1 className="text-4xl font-lulo">POR QUE É LIGHTWATTS?</h1>
         <p className="text-lg mt-4 w-2/3 text-justify">
-          Nossa missão é ajudar você a economizar energia de forma inteligente, monitorando o consumo dos seus aparelhos eletrônicos em tempo real. Acreditamos que pequenas mudanças no uso diário dos dispositivos podem gerar grandes economias ao final do mês. Com o nosso sistema de gerenciamento, você terá um controle completo sobre o consumo de cada equipamento em sua casa, identificando desperdícios e tomando decisões mais conscientes.
+        Nosso nome é LightWatts porque acreditamos que a luz é sinônimo de clareza, eficiência e inovação. Queremos iluminar o caminho para um consumo de energia mais inteligente e sustentável, ajudando você a economizar watts de forma leve e eficiente. Com nossas soluções, seu lar fica mais econômico, sem perder o conforto, até porque economizar energia pode ser tão simples quanto acender uma luz.
         </p>
       </article>
-      <section className="flex mt-28 w-screen bg-gray-100 p-8">
+      <section className="flex mt-28 text-black w-screen bg-gray-100 p-8">
         <div>
           <div className="ml-10">
             <Link href="/auth/signup">
@@ -35,8 +37,8 @@ export default function Home() {
               </h1>
             </Link>
           </div>
-          <p className="ml-10 w-1/2 text-2xl mt-5">
-            Aqui, ninguém será o vilão da sua história, então fique tranquilo!
+          <p className="ml-10 w-3/4 text-xl mt-5 text-justify">
+          Aqui, ninguém será o vilão da sua história, então relaxe! Economizar energia não exige sacrifícios extremos ou culpa. É uma jornada leve e consciente, onde pequenas mudanças consistentes podem reduzir sua conta de luz e beneficiar o planeta.
           </p>
           <Link href="/auth/signup">
             <p className="loginlink text-xl inline-block mt-8 ml-10">
@@ -46,8 +48,7 @@ export default function Home() {
         </div>
         <Image className="mr-24" src="/img/tomada.jpg" alt="Tomada" width={500} height={300} />
       </section>
-
-      <section className="flex bg-black opacity-90 p-8 mt-16">
+      <section className="flex bg-black opacity-90 p-8">
         <Image className="ml-8" src="/img/girl_in_phone.jpg" alt="Garota no celular" width={500} height={500} />
         <div className="ml-20 space-y-5">
           <Link href="/fale-conosco">
@@ -55,7 +56,7 @@ export default function Home() {
               Entre em contato conosco caso haja alguma pendência.
             </h1>
           </Link>
-          <p className="text-white text-xl">
+          <p className="text-white text-xl text-justify">
             Se houver alguma pendência ou dúvida, não hesite em nos contatar. Estamos à disposição para ajudar e resolver qualquer questão o mais rápido possível.
           </p>
           <Link href="/fale-conosco">
@@ -65,30 +66,8 @@ export default function Home() {
           </Link>
         </div>
       </section>
-
-      <section id="testimonials" className="py-24 bg-gray-50">
-        <div className="max-w-5xl mx-auto text-center px-6 lg:px-0">
-          <h3 className="text-5xl font-bold text-gray-800 mb-16">Depoimentos</h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-            <blockquote className="bg-white p-8 shadow-xl rounded-lg transform transition-all duration-300 hover:shadow-2xl hover:-translate-y-2">
-              <p className="text-lg text-gray-700 mb-6">
-                "Economizei 30% na minha conta de luz usando o LightWatts! A plataforma é intuitiva e fácil de usar."
-              </p>
-              <div className="flex items-center justify-center gap-4">
-                <cite className="text-gray-900 font-semibold">João Silva</cite>
-              </div>
-            </blockquote>
-            <blockquote className="bg-white p-8 shadow-xl rounded-lg transform transition-all duration-300 hover:shadow-2xl hover:-translate-y-2">
-              <p className="text-lg text-gray-700 mb-6">
-                "Agora tenho controle total sobre o consumo de energia da minha casa. LightWatts realmente faz a diferença!"
-              </p>
-              <div className="flex items-center justify-center gap-4">
-                <cite className="text-gray-900 font-semibold">Maria Oliveira</cite>
-              </div>
-            </blockquote>
-          </div>
-        </div>
-      </section>
+      <CardSlider/>
+      <Footer/>
     </div>
   );
 }
